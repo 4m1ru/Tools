5396e25f121ca78211a8314919c945658779180f
M4FTU5U7 <63690507+M4FTU5U7@users.noreply.github.com>
2020-04-15 09:24:19 +0700

    Add files via upload

---
+++ hack.py
@@ -0,0 +1,140 @@
+import os
+import sys
+import time
+os.system('clear')
+
+def th(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1./12)
+th("\033[0;32mHAI Selamat Datang di MAFTUHUL CAHNEL")
+time.sleep(2)
+se = input("\033[0;32mSIAPA NAMA KAMU? :")
+print ("\033[0;32mSELAMAT DATANG DI PROGRAM KAMI",se)
+time.sleep(2)
+os.system('clear')
+th("\033[0;32mJANGA DI SALAH GUNAKAN SCRIPT INI")
+th("\033[0;32mjangan lupa SUBSCRIBE Like and coment ")
+time.sleep(1)
+os.system ('clear')
+th("SUBSCRIBE DULU...")
+time.sleep(1)
+th(". . . . . . . . . . . . . .")
+time.sleep(3)
+os.system('clear')
+th("\033[0;32mLOADING...")
+time.sleep(2)
+th("\033[0;32m> > > > > > > > > > > > > ] 100%")
+time.sleep(1)
+os.system('clear')
+def main():
+    password = input ('\033[0;32mMasukan password : ')
+    if password == 'maptuyul':
+         print ('\033[0;32mkata sandi benar')
+         time.sleep(2)
+         os.system('clear')
+    else:
+        print ('\033[1;31mkata sandi salah')
+main()
+os.system ('figlet Tools')
+banner = """
+\033[1;33m========================================
+Author = \033[1;33mMaftuhul
+Youtube = \033[1;33mMaftuhul uzy
+github = \033[1;33mpembuatan
+\033[1;33m========================================
+"""
+print (banner)
+print ("\033[0;32m[√] \033[1;33mMenu pilihan [√]")
+print ("\033[0;32m[1] \033[1;33mDark Fb ")
+print ("\033[0;32m[2] \033[1;33mspamm wa ")
+print ("\033[0;32m[3] \033[1;33mspamm call ")
+print ("\033[0;32m[4] \033[1;33mTrojans ")
+print ("\033[0;32m[5] \033[1;33mSpam SMS ")
+print ("\033[0;32m[6] \033[1;33mDdos Att ")
+print ("\033[1;32m[7] \033[1;33mHammer ")
+print ("\033[1;32m[8] \033[1;33mtorsHammer")
+print ("\033[1;32m[9] \033[1;33mHack ig dan fb")
+print ("\033[1;32m[10] \033[1;33mBahan bahan")
+print ("\033[1;31m[99] \033[1;33m exit ")
+pilih = input('\033[0;32m[?] \033[1;33m pilih : ')
+if pilih == "1":
+        os.system ('git clone https://github.com/storiku/darkfb')
+        print (" $cd darkfb ")
+        print (" $python2 darkfb.py ")
+        time.sleep(2)
+
+if pilih == "2":
+        os.system ('git clone https://github.com/sandiwijayani1/SpamWa')
+        print (" $ cd SpamWa ")
+        print (" $ python3 spam.py ")
+        time.sleep(2)
+
+if pilih == "3":
+        os.system ('git clone https://github.com/Fukur0-3XP/SpamCall')
+        print (" $ cd SpamCall ")
+        print (" $ python2 Call.py ")
+        time.sleep(2)
+
+if pilih == "4":
+        os.system ('git clone https://github.com/R133F/Trojans')
+        print ("$ cd Trojans ")
+        print ("$ paython2 Trojans.py ")
+        time.sleep(2)
+
+if pilih == "5":
+        os.system ('git clone https://github.com/Xractz/SMS')
+        print (" $ cd SMS ")
+        print (" $ python spam.py")
+        time.sleep(2)
+
+if pilih == "6":
+        os.system ('git clone https://github.com/BPCATTACKER/DOS-TOOKITS')
+        print (" $ cd DOS-TOOKITS ")
+        print (" $ chmod +x DOS-TOOKITS.sh")
+        print (" $  sh DoS-TOOKIT.sh ")
+        time.sleep(2)
+
+if  pilih == "7":
+        os.system ('git clone https://github.com/cyweb/hammer')
+        print (" $ cd hammer")
+        print (" $ chmod +x hammer.py")
+        print (" $ python hammer.py")
+        print (" $ python3 hammer.py -s [IP target] -p [80] -t [135")
+        time.sleep(2)
+
+if pilih == "8":
+        os.system (' git clone https://github.com/dotfighter/torshammer.git')
+        print (" $ cd torshammer")
+        print (" $ chmod +x torshammer.oy")
+        print (" $ python2 torshammer.py")
+        time.sleep(2)
+
+if pilih == "9":
+        os.system (' git clone https://github.com/MrTamfanX/TamfanFacebook')
+        print (" $ cd TamfanFacebook")
+        print (" $ chmod +x *")
+        print (" chmod -R 775 lib")
+        print (" sh install.sh")
+        print (" $ sh tamfan.sh")
+        time.sleep(2)
+
+if pilih == "10":
+        os.system ('pkg update && apt upgrade')
+        os.system ('pkg Install git')
+        os.system ('pkg install nano')
+        os.system ('pkg install python2')
+        os.system ('pkg install bash')
+        os.system ('apt install toilet')
+        os.system ('pkg install python')
+        os.system ('apt install lolcat')
+        os.system ('apt install tor')
+        os.system ('apt install php git')
+        print ("[√] pengistalan selesai ")
+        time.sleep(2)
+
+if pilih == "99":
+        os.system ("figlet Subscribe")
+        time.sleep(1)
+        os.system ('clear')
